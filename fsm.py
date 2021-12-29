9d314b0fef75b1a8ca22a3c01b8a164d0ea50b9f
williamlin0518 <80030403+williamlin0518@users.noreply.github.com>
2021-12-30 06:47:42 +0800

    test

--- fsm.py
+++ fsm.py
@@ -1,3 +1,5 @@
+import string
+
 import requests
 from transitions.extensions import GraphMachine
 
@@ -7,15 +9,16 @@ from bs4 import BeautifulSoup
 location = ""
 url = 'https://www.imdb.com/search/title/?count=100&groups=top_1000&sort=user_rating'
 
+
 class TocMachine(GraphMachine):
     def __init__(self, **machine_configs):
         self.machine = GraphMachine(model=self, **machine_configs)
 
     def is_going_to_state1(self, event):
         text = event.message.text
-        return text.lower() == "1"             # user to state1
+        return text.lower() == "1"  # user to state1
 
-    def on_enter_state1(self, event):         # when enter state1
+    def on_enter_state1(self, event):  # when enter state1
         print("I'm entering state1")
 
         title = '看啥電影'
@@ -33,20 +36,18 @@ class TocMachine(GraphMachine):
         url = 'https://i.imgur.com/8Vvxj0S.jpg'
         send_button_message(event.reply_token, title, text, btn, url)
 
-
-
-
     def is_going_to_state2(self, event):
         text = event.message.text
         return text.lower() == "科幻"
 
-
     def on_enter_state2(self, event):
         print("I'm entering state2")
         response = requests.get(url)
-        soup = BeautifulSoup(response.content, 'html.parser')
-        send_text_message(event.reply_token, soup)
 
+        BeautifulSoup(response.content, 'html.parser')
+        soup = BeautifulSoup(response.content, 'html.parser')
+        souptext = soup[0:400]
+        send_text_message(event.reply_token, souptext)
 
     def on_exit_state2(self):
         print("Leaving state2")
